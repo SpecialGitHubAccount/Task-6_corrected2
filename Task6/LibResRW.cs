a898b7c1b753f4f9cb9fed565f1b1508e01118ee
Islam <islamrahimbaeff@outlook.com>
2018-08-25 18:01:53 +0600

    6-th

---
+++ Task6/LibResRW.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using Task6;
+
+namespace Task6
+{
+    public class LibResRW : LibraryInfrastructure<LibraryResource>, IDisposable
+    {
+        
+        public LibResRW(Stream stream)
+        {
+            this.stream = stream;
+            // Если корневого элемента не будет найдено - возникнет исключение.
+            try
+            {
+                doc = XDocument.Load(stream);
+            }
+            catch
+            {
+                doc = new XDocument(new XElement("catalog"));
+                doc.Save(stream);
+            }
+            // Если не будет найдено тега "Каталог",
+            if (doc.Element("catalog") == null)
+            {
+                doc = new XDocument(new XElement("catalog"));
+                doc.Save(stream);
+            }
+
+            stream.Position = 0;
+            reader = XmlReader.Create(stream);
+            writer = XmlWriter.Create(stream);
+        }
+
+        public IEnumerable<LibraryResource> Read()
+        {
+            stream.Position = 0;
+            reader = XmlReader.Create(stream);
+            reader.ReadToFollowing(XmlRootElelement);
+            XElement catalog = XElement.ReadFrom(reader) as XElement;
+            List<LibraryResource> libResources = new List<LibraryResource>();
+            
+
+            catalog.Elements().ToList().ForEach((libResourceXElement) =>
+            {
+                LibraryResource libraryResource = (LibraryResource)ObjectCreator.CreateFromXElementIfMatch(libResourceXElement, this.GetType().Namespace);
+                libResources.Add(libraryResource);                
+            });
+            return libResources;
+        }
+
+        public void Write(LibraryResource item)
+        {
+            XElement data = item.SerializeToXEelement();
+            doc.Element(XmlRootElelement).Add(data);
+        }
+
+        public void Flush()
+        {
+            stream.Position = 0;
+            doc.Save(stream);
+            stream.Position = 0;
+            doc = XDocument.Load(stream);
+        }
+
+        public void Dispose()
+        {
+            stream.Position = 0;
+            doc.Save(stream);
+            reader.Close();
+            writer.Close();
+            stream.Close();
+        }
+
+        private XmlReader reader;
+        private XmlWriter writer;
+        private XDocument doc;
+        private readonly Stream stream;
+        
+        private static string XmlRootElelement = "catalog";
+    }
+}
---
+++ Task6/ObjectCreator.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Task6
+{
+    public static class ObjectCreator
+    {
+
+        public static object CreateFromXElementIfMatch(XElement item, string ns)
+        {
+            Type entityType = Assembly.GetExecutingAssembly().GetTypes()
+                .FirstOrDefault(ti => ti.Namespace == ns && ti.Name == item.Name);
+            if(entityType == null)
+            {
+                throw new Exception();
+            }
+            object entity = Activator.CreateInstance(entityType);
+            Int32Setter(entity, item);
+            Int64Setter(entity, item);
+            StringsSetter(entity, item);
+            DateSetter(entity, item);
+            return entity;
+        }
+
+        private static object Int64Setter( object libResource, XElement item)
+        {
+            foreach (PropertyInfo property in libResource.GetType().GetProperties().Where(pi => pi.PropertyType == typeof(Nullable<Int64>)))
+            {
+                long? value = null;
+                try
+                {
+                    value = XmlConvert.ToInt64(item.Attribute(property.Name).Value);
+                }
+                catch
+                {
+                    value = null;
+                }
+                if (Attribute.IsDefined(property, typeof(RequiredAttribute), true))
+                {
+                    if (!value.HasValue)
+                    {
+                        throw new Exception("this is mandatory field");
+                    }
+                }
+                property.SetValue(libResource, value);
+            }
+            return libResource;
+        }
+
+        private static object Int32Setter(object libResource, XElement item)
+        {
+            foreach (PropertyInfo property in libResource.GetType().GetProperties().Where(pi => pi.PropertyType == typeof(Nullable<Int32>)))
+            {
+                int? value = null;
+                try
+                {
+                    value = XmlConvert.ToInt32(item.Attribute(property.Name).Value);
+                }
+                catch
+                {
+                    value = null;
+                }
+                if (Attribute.IsDefined(property, typeof(RequiredAttribute), true))
+                {
+                    if (!value.HasValue)
+                    {
+                        throw new Exception("this is mandatory field");
+                    }
+                }
+                property.SetValue(libResource, value);
+            }
+            return libResource;
+        }
+
+        private static object StringsSetter(object libResource, XElement item)
+        {
+            foreach (PropertyInfo property in libResource.GetType().GetProperties().Where(pi => pi.PropertyType == typeof(string)))
+            {
+                var value = item.Attribute(property.Name);
+                if (Attribute.IsDefined(property, typeof(RequiredAttribute), true))
+                {
+                    if (string.IsNullOrEmpty(value.ToString()))
+                    {
+                        throw new Exception("this is mandatory field");
+                    }
+                }
+                // а если не равен null - можно будет безопасно возпользоваться
+                // свойством Value, объекта value.
+                if (value != null)
+                {
+                    property.SetValue(libResource, value.Value);
+                }
+            }
+            return libResource;
+        }
+
+        private static object DateSetter(object libResource, XElement item)
+        {
+            foreach (PropertyInfo property in libResource.GetType().GetProperties().Where(pi => pi.PropertyType == typeof(Nullable<DateTime>)))
+            {
+                DateTime? value = null;
+                try
+                {
+                    value = XmlConvert.ToDateTime(item.Attribute(property.Name).Value, XmlDateTimeSerializationMode.Local);
+                }
+                catch
+                {
+                    value = null;
+                }
+                if (Attribute.IsDefined(property, typeof(RequiredAttribute), true))
+                {
+                    if (!value.HasValue)
+                    {
+                        throw new Exception("this is mandatory field");
+                    }
+                }
+                property.SetValue(libResource, value);
+            }
+            return libResource;
+        }
+    }
+}
